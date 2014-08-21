aa27b3417e9d97fc9efca4689936702bf4ff55c0
Mark Chapman <mark.chapman@gmail.com>
2014-08-21 01:23:33 +0100

    Files can now be added and saved to the collection, but not retrieved / deleted etc

--- dist/fng-jq-upload.js
+++ dist/fng-jq-upload.js
@@ -4,9 +4,10 @@
     'blueimp.fileupload'
   ]);
 
-  app.directive('fngJqUploadForm', ['$rootScope', 'fileUpload', function () {
+  app.directive('fngJqUploadForm', ['fileUpload', function () {
     return {
       link: function (scope, element, attrs) {
+        scope.options = {};
         // Pick up options from the mongoose schema
         scope.formScope = scope.$parent;
         scope.passedParams = scope.formScope[attrs.schema];
@@ -14,106 +15,47 @@
         scope.url='/file/upload/' + scope.formScope.modelName;
         scope.options.url = scope.url;
         scope.options.maxFileSize = scope.sizeLimit;
+        scope.name = scope.passedParams.name;
       },
       restrict: 'E',
       templateUrl: 'templates/fileform.html',
-      scope: {
-        allowed: '@',
-        url: '@',
-        autoUpload: '@',
-        sizeLimit: '@',
-        name: '@'
-      },
-//            case 'fileuploader':
-//              sizeClassBS3 = 'col-xs-12';
-//              value = '<ng-upload-form url="/file/upload/' + scope.modelName + '" auto-upload="true" size-limit="50000000" name="'+ idString +'"></ng-upload-form>';
-//              break;
-
-      controller: function ($rootScope, $scope, $element, fileUpload) {
-
-        $scope.options = {
-          url: $scope.url,
-          dropZone: $element,
-          maxFileSize: $scope.sizeLimit,
-          autoUpload: $scope.autoUpload
-        };
+      scope: {},
+      controller: function ($scope, $element, fileUpload) {
         $scope.loadingFiles = false;
-        if (!$scope.queue) {
-          $scope.queue = [];
-        }
+        $scope.formScope = $scope.$parent;
 
-        var generateFileObject = function generateFileObjects(objects) {
-          angular.forEach(objects, function (fileObject, key) {
-            if (fileObject.url && fileObject.url.charAt(0) !== '/') {
-              fileObject.url = '/' + fileObject.url;
-            }
-
-            if (fileObject.deleteUrl && fileObject.deleteUrl.charAt(0) !== '/') {
-              fileObject.deleteUrl = '/' + fileObject.deleteUrl;
-            }
-
-            if (fileObject.thumbnailUrl && fileObject.thumbnailUrl.charAt(0) !== '/') {
-              fileObject.thumbnailUrl = '/' + fileObject.thumbnailUrl;
-            }
-
-            $scope.queue[key] = fileObject;
-          });
-        };
-
-        //extend fileUpload
-        fileUpload.fieldData = {};
-        fileUpload.addFieldData = function addFieldData(fieldName, fileData) {
-          fieldData[fieldName].push(fileData);
-        };
-        fileUpload.removeFieldData = function removeFieldData(fieldName, fileId) {
-          angular.forEach(fieldData[fieldName], function (value, key) {
-            if (value && value._id) {
-              if (value._id === fileId) {
-                fieldData[fieldName].splice(key, 1);
-              }
-            }
-          });
-        };
-        fileUpload.registerField = function registerField(fieldName) {
-          if (!fieldData[fieldName]) {
-            fieldData[fieldName] = [];
-          }
-        };
-//
-//
-//
-//        $scope.filequeue = fileUpload.fieldData[$scope.name];
-
-        $scope.$watchCollection('filequeue', function (newval) {
-          generateFileObject(newval);
-//          $scope.$parent.$parent.$parent.$parent.record[$scope.fieldname] = $scope.filequeue;
+        $scope.$on('fileuploaddone', function(event, data) {
+          $scope.formScope.record[$scope.name] = $scope.formScope.record[$scope.name] || [];
+          var fileDetails = data.result.files[0];
+          var breakdown = /\/file\/(.+?)\/([0-9a-f]{24})/.exec(fileDetails.url);
+          $scope.formScope.record[$scope.name].push(
+            {
+              _id:      breakdown[2],
+              id:       breakdown[2],
+              filename: fileDetails.name,
+              size:     fileDetails.size
+            });
         });
-
-        $scope.$watchCollection('queue', function (newval) {
-//          generateFileObject(newval);
-//          $scope.$parent.$parent.$parent.$parent.record[$scope.fieldname] = $scope.filequeue;
-        });
-
       }
     };
   }])
-    .controller('FileDestroyController', ['$rootScope', '$scope', '$http', 'fileUpload', function ($rootScope, $scope, $http, fileUpload) {
+    .controller('FileDestroyController', ['$scope', '$http', 'fileUpload', function ($scope, $http, fileUpload) {
       var file = $scope.file,
         state;
 
-      $scope.uploadScope = $scope.$parent.$parent.$parent;
-      $scope.formScope = $scope.uploadScope.$parent;
-      $scope.name = $scope.uploadScope.passedParams.name;
-
-      if (!fileUpload.fieldData) {
-        fileUpload.fieldData = {};
-      }
-
-      if (!fileUpload.fieldData[$scope.name]) {
-        fileUpload.fieldData[$scope.name] = [];
-      }
-
-      $scope.filequeue = fileUpload.fieldData;
+//      $scope.uploadScope = $scope.$parent.$parent.$parent;
+//      $scope.formScope = $scope.uploadScope.$parent;
+//      $scope.name = $scope.uploadScope.passedParams.name;
+//
+//      if (!fileUpload.fieldData) {
+//        fileUpload.fieldData = {};
+//      }
+//
+//      if (!fileUpload.fieldData[$scope.name]) {
+//        fileUpload.fieldData[$scope.name] = [];
+//      }
+//
+//      $scope.filequeue = fileUpload.fieldData;
 
       if (file.url) {
         file.$state = function () {
@@ -127,17 +69,13 @@
           }).then(
             function () {
               state = 'resolved';
-              fileUpload.removeFieldData($scope.fieldname, file.result._id);
               $scope.clear(file);
             },
             function () {
               state = 'rejected';
-              fileUpload.removeFieldData($scope.fieldname, file.result._id);
               $scope.clear(file);
             }
           );
-
-
         };
       } else if (!file.$cancel && !file._index) {
         file.$cancel = function () {

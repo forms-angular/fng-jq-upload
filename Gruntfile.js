63a0b5677267c43dbf12a0a4ed5730f53bea29c5
Mark Chapman <mark.chapman@gmail.com>
2017-02-01 12:14:25 +0000

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-        jshintrc: '.jshintrc'
+        jshintrc: '.jshintrc',
+        reporterOutput: '',
       },
       all: [
         'Gruntfile.js',
